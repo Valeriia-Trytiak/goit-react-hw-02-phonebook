00cfd5138e2fad9c9b33d5fc8ebbc9594a2fa113
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-10-08 17:42:13 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,11 +3,8 @@ import { Formik, Form, Field, ErrorMessage  } from 'formik';
 import * as Yup from 'yup';
 
   const ContactShema = Yup.object().shape({
-    name: Yup.string().min(2, 'Too Short!')
-    .matches(/^[a -zA-Zа-яА-Я]+([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*$/, "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan")
-    .required('This is a required field'), 
-    number: Yup.string().matches(/^\+?\d{1,4}?[ .\-s]?(\(\d{1,3}?\))?([ .\-s]?\d{1,4}){1,4}$/, "Phone number must be digits and can contain spaces, dashes, parentheses and can start with +")
-    .required('This is a required field').min(9, "Please enter at least 9 characters")
+    name: Yup.string().min(2, 'Too Short!').matches(/^[a -zA-Zа-яА-Я]+([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*$/, "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan").required('This is a required field'), 
+    number: Yup.string().matches(/^\+?\d{1,4}?[ .\-s]?(\(\d{1,3}?\))?([ .\-s]?\d{1,4}){1,4}$/, "Phone number must be digits and can contain spaces, dashes, parentheses and can start with +").required('This is a required field').min(9, "Please enter at least 9 characters")
 });
 
 export class ContactForm extends Component {
@@ -21,7 +18,6 @@ export class ContactForm extends Component {
             actions.resetForm();
         }}
 
-        
         validationSchema={ContactShema}
       >
         <Form>
